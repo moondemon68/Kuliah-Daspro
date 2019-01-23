7189a04d6f151d1a8a64924f440f873943f666e1
Morgen Sudyanto <moondemon68@gmail.com>
2019-01-23 11:52:23 +0700

    Create Kali.hs

---
+++ Praktikum-0/Kali.hs
@@ -0,0 +1,15 @@
+--NIM/Nama: 16518380/Morgen Sudyanto
+--Nama file: Kali.hs
+--Topik: Ekspresi Dasar
+--Tanggal: 23 Januari 2019
+--Deskripsi: Mengalikan dua buah bilangan
+
+module Kali where
+--Perkalian dua bilangan 
+--Definisi dan Spesifikasi
+kali::Int->Int->Int
+	--kali a b menghasilkan a*b
+--Realisasi
+kali a b=a*b
+--Contoh aplikasi
+--kali 3 4
