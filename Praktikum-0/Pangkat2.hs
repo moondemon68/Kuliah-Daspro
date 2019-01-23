264c31d9eddc612edc8a544dc980b40090c7e447
Morgen Sudyanto <moondemon68@gmail.com>
2019-01-23 11:52:43 +0700

    Create Pangkat2.hs

---
+++ Praktikum-0/Pangkat2.hs
@@ -0,0 +1,16 @@
+--NIM/Nama: 16518380/Morgen Sudyanto
+--Nama file: Pangkat2.hs
+--Topik: Ekspresi Dasar
+--Tanggal: 23 Januari 2019
+--Deskripsi: Mengkuadratkan sebuah bilangan
+
+module Pangkat2 where
+--Pangkat2 - pangkat2(x)
+--Definisi dan Spesifikasi
+pangkat2::Int->Int
+	--pangkat2(x) menghitung pangkat dua dari x,
+	--sebuah bilangan integer
+--Realisasi
+pangkat2 x=x*x
+--Contoh aplikasi
+--pangkat2 2
