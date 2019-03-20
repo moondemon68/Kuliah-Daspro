9a891f8bc3fcb8013d99419ed0440625409402a7
Morgen Sudyanto <moondemon68@gmail.com>
2019-03-20 12:19:32 +0700

    Update themepark.pas

--- Praktikum-4/themepark.pas
+++ Praktikum-4/themepark.pas
@@ -11,22 +11,34 @@ var
 //ALGORITMA
 begin
 	readln(t,b,k);
-	if ((k=1) and (t>100) and (b<=150)) then
+	if ((k=2) and (t>100) and (b<=150)) then
 		writeln('TRUE')
-	else if ((k<>1) and (t>100) and (b<=150)) then
+	else if ((k=3) and (t>100) and (b<=150)) then
+		writeln('TRUE')
+	else if ((k=4) and (t>100) and (b<=150)) then
+		writeln('TRUE')
+	else if ((k=0) and (t>100) and (b<=150)) then
 		writeln('FALSE')
-	else if ((k=2) and (t<=30) and (b<=150)) then
+	else if ((k=1) and (t>100) and (b<=150)) then
+		writeln('FALSE')
+	//END OF 1
+	else if ((k=2) and (t<=100) and (b>30) and (b<=150)) then
 		writeln('TRUE')
 	else if ((k=1) and (t<=100) and (b<=150)) then
 		writeln('TRUE')
+	//END OF 2
 	else if ((k=2) and (b>150) and (t<=100)) then
 		writeln('TRUE')
 	else if ((k=3) and (b>150) and (t>100) and (t<=200)) then
 		writeln('TRUE')
+	else if ((k=2) and (b>150) and (t>100) and (t<=200)) then
+		writeln('TRUE')
+	//END OF 3
 	else if (k=0) then
 		writeln('TRUE')
 	else if (k<>0) then
 		writeln('FALSE');
+	//END OF 4
 	end.
 			
 end.
